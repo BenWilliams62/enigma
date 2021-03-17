a764527b2b42e1d3aabd3089bebb0fbd9ed177a9
Ben <benwilliams6299@gmail.com>
2021-03-17 10:34:24 +0000

    settings.json added

--- enigma.py
+++ enigma.py
@@ -1,60 +1,34 @@
 from functions import (
     input_message,
-    char_to_number,
-    number_to_char,
     plug,
     reflect,
-    rotor_1,
-    rotor_2,
-    rotor_3
+    rotor_function
 )
+import json
+
 
 
 def main():
-    # plug board - change the keys to desired plugs.
-    # Use every character once as a key and once as a value
-    # Set this regularly
-    plug_board = {
-        "a":"z",
-        "b":"y",
-        "c":"x",
-        "d":"w",
-        "e":"v",
-        "f":"u",
-        "g":"t",
-        "h":"s",
-        "i":"r",
-        "j":"q",
-        "k":"p",
-        "l":"o",
-        "m":"n",
-        "n":"m",
-        "o":"l",
-        "p":"k",
-        "q":"j",
-        "r":"i",
-        "s":"h",
-        "t":"g",
-        "u":"f",
-        "v":"e",
-        "w":"d",
-        "x":"c",
-        "y":"b",
-        "z":"a"
-    }
-    # Set these switches irregularly
-    rotor_1_switch = 5
-    rotor_2_switch = 2
-    rotor_3_switch = 13
+    # read in data from settings.json
+    with open('./settings.json') as f:
+        data = json.load(f)
+    
+    # set settings
+    plug_board = data["plugBoard"]              # plugboard
+    rotor_1_switch = data["rotorSwitch1"]       # rotor switch 1
+    rotor_2_switch = data["rotorSwitch2"]       # rotor switch 2
+    rotor_3_switch = data["rotorSwitch3"]       # rotor switch 3
+    rotor_1_setting = data["rotor1"]            # rotor 1
+    rotor_2_setting = data["rotor2"]            # rotor 2
+    rotor_3_setting = data["rotor3"]            # rotor 3
+    rotor_1_index = data["rotor1Index"]         # rotor 1 starting position
+    rotor_2_index = data["rotor2Index"]         # rotor 2 starting position
+    rotor_3_index = data["rotor3Index"]         # rotor 3 starting position
+    
 
     # dont touch values below here
-    output = ""                     # sets output as empty string to append values to
-    print("\nrotor settings:\n")
-    rotor_1_index = int(input("enter the number for rotor 1's starter position:\n"))   # starting value
-    rotor_2_index = int(input("enter the number for rotor 2's starter position:\n"))   # starting value
-    rotor_3_index = int(input("enter the number for rotor 3's starter position:\n"))   # starting value
-    print("\n\n")
     input_value = input_message()   # takes an input string
+    output = ""                     # sets output as empty string to append values to
 
     for char in input_value:
         
@@ -63,40 +37,37 @@ def main():
         else:
             # put through plug board
             plugged = plug(char, plug_board)
-
-            # convert to number for rotor usage
-            num = char_to_number(plugged)
+            # z - s
 
             # go through rotor 1
-            rot_1 = rotor_1(num, rotor_1_index, "f")
+            rot_1 = rotor_function(plugged, rotor_1_index, "f", rotor_1_setting)
 
             # go through rotor 2
-            rot_2 = rotor_2(rot_1, rotor_2_index, "f")
+            rot_2 = rotor_function(rot_1, rotor_2_index, "f", rotor_2_setting)
 
             # go through rotor 3
-            rot_3 = rotor_3(rot_2, rotor_3_index, "f")
+            rot_3 = rotor_function(rot_2, rotor_3_index, "f", rotor_3_setting)
 
             # reflect
             reflection = reflect(rot_3)
 
             # go through rotor 3 backwards
-            rot_3_ref = rotor_3(reflection, rotor_3_index, "b")
+            rot_3_ref = rotor_function(reflection, rotor_3_index, "b", rotor_3_setting)
 
             # go through rotor 2 backwards
-            rot_2_ref = rotor_2(rot_3_ref, rotor_2_index, "b")
+            rot_2_ref = rotor_function(rot_3_ref, rotor_2_index, "b", rotor_2_setting)
 
             # go through rotor 1 backwards
-            rot_1_ref = rotor_1(rot_2_ref, rotor_1_index, "b")
-
-            # convert number to character
-            return_char = number_to_char(rot_1_ref)
+            rot_1_ref = rotor_function(rot_2_ref, rotor_1_index, "b", rotor_1_setting)
 
             # back through plug board
-            return_plug = plug(return_char, plug_board)
+            return_plug = plug(rot_1_ref, plug_board)
+            
 
 
             # add value to output
             output += return_plug
+            # h - a
             
             # add new setting for the rotors
             rotor_1_index += 1
--- functions.py
+++ functions.py
@@ -1,144 +1,30 @@
 # enigma machine
 
-def plug(char, plug_board):
-    return plug_board[char]
-
-# char to number
-def char_to_number(char):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    index = alphabet.index(char)
-    return index
-
-# number to char
-def number_to_char(num):
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    return alphabet[num]
-
-
-# reflector - find index of letter, then return opposite (25 - index)
-def reflect(num):
-    reflection = 25 - num
-    return reflection
-
-
 # input
 def input_message():
     message = str(input("input message:\n")).lower()
     return message
 
-# rotor 1
-def rotor_1(char, step, direction):
-    # set switcher once and forget it
-    switcher = {
-        0:2,
-        1:4,
-        2:6,
-        3:8,
-        4:10,
-        5:12,
-        6:14,
-        7:16,
-        8:18,
-        9:20,
-        10:22,
-        11:24,
-        12:1,
-        13:3,
-        14:5,
-        15:7,
-        16:9,
-        17:11,
-        18:13,
-        19:15,
-        20:17,
-        21:19,
-        22:21,
-        23:23,
-        24:25,
-        25:0
-    }
-    if direction == "f":
-        val = ((char + step) + 26) % 26
-        return switcher[val]
-    else:
-        for k, v in switcher.items():
-            if v == char:
-                return (k - step + 26) % 26
+# plug board
+def plug(char, plug_board):
+    return plug_board[char]
 
-# rotor 2
-def rotor_2(char, step, direction):
-    # set switcher once and forget it
-    switcher = {
-        0:3,
-        1:6,
-        2:9,
-        3:12,
-        4:15,
-        5:18,
-        6:21,
-        7:24,
-        8:2,
-        9:5,
-        10:8,
-        11:11,
-        12:14,
-        13:17,
-        14:0,
-        15:23,
-        16:1,
-        17:4,
-        18:7,
-        19:10,
-        20:13,
-        21:16,
-        22:19,
-        23:22,
-        24:25,
-        25:20
-    }
+# rotor function
+def rotor_function(char, step, direction, switcher):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    index = alphabet.index(char)
     if direction == "f":
-        val = ((char + step) + 26) % 26
-        return switcher[val]
+        val = ((index + int(step)) + 26) % 26
+        return switcher[alphabet[val]]
     else:
         for k, v in switcher.items():
             if v == char:
-                return (k - step + 26) % 26
+                index_k = alphabet.index(k)
+                return alphabet[(index_k - int(step) + 26) % 26]
 
-# rotor 3
-def rotor_3(char, step, direction):
-    # set switcher once and forget it
-    switcher = {
-        0:4,
-        1:8,
-        2:12,
-        3:16,
-        4:20,
-        5:24,
-        6:0,
-        7:7,
-        8:11,
-        9:15,
-        10:19,
-        11:23,
-        12:2,
-        13:6,
-        14:10,
-        15:14,
-        16:18,
-        17:22,
-        18:1,
-        19:5,
-        20:9,
-        21:13,
-        22:17,
-        23:21,
-        24:25,
-        25:3
-    }
-    if direction == "f":
-        val = ((char + step) + 26) % 26
-        return switcher[val]
-    else:
-        for k, v in switcher.items():
-            if v == char:
-                return (k - step + 26) % 26
+# reflector - find index of letter, then return opposite (25 - index)
+def reflect(char):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    index = alphabet.index(char)
+    reflection = 25 - index
+    return alphabet[reflection]
